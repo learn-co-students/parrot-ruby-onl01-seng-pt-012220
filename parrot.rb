274abe6f34f197eda680e0fd4d3cbc1bde6d5f8b
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2020-01-28 03:55:10 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  phrase = "Squawk!"
+end
+
+def parrot (phrase = "Squawk!")
+  chirp = phrase
+  puts (phrase)
+  chirp
+end
